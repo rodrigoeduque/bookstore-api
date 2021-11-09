a20d4668e5e487a5e6331a94eda30f59f623a786
Rodrigo Eustáquio Duque <rodrigoeduque@hotmail.com>
2021-11-09 08:48:42 -0300

    fix: ajust imports

--- src/main/java/br/com/rodrigoeduque/bookstore/domain/Categoria.java
+++ src/main/java/br/com/rodrigoeduque/bookstore/domain/Categoria.java
@@ -1,6 +1,9 @@
 package br.com.rodrigoeduque.bookstore.domain;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
