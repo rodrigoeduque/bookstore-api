061a8278a512a0b967dfe9eebcd14ee6cf120dd7
Rodrigo Eustáquio Duque <rodrigoeduque@hotmail.com>
2021-11-16 18:07:37 -0300

    feat: tratamento validações

--- src/main/java/br/com/rodrigoeduque/bookstore/controller/exceptions/ControllerExceptionHandler.java
+++ src/main/java/br/com/rodrigoeduque/bookstore/controller/exceptions/ControllerExceptionHandler.java
@@ -2,8 +2,11 @@ package br.com.rodrigoeduque.bookstore.controller.exceptions;
 
 import br.com.rodrigoeduque.bookstore.services.exceptions.DataIntegrityViolationException;
 import br.com.rodrigoeduque.bookstore.services.exceptions.ObjectNotFoundException;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -21,6 +24,15 @@ public class ControllerExceptionHandler {
   @ExceptionHandler(DataIntegrityViolationException.class)
   public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException e, ServletRequest request) {
     StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), e.getMessage());
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+  }
+
+  @ExceptionHandler(ConstraintViolationException.class)
+  public ResponseEntity<StandardError> validationErrors(MethodArgumentNotValidException e, ServletRequest request) {
+    ValidationError error = new ValidationError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Erro na validação dos campos");
+    for (FieldError x : e.getBindingResult().getFieldErrors()){
+      error.addErrors(x.getField(),x.getDefaultMessage());
+    }
     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
   }
 
--- src/main/java/br/com/rodrigoeduque/bookstore/controller/exceptions/ValidationError.java
+++ src/main/java/br/com/rodrigoeduque/bookstore/controller/exceptions/ValidationError.java
@@ -1,2 +1,24 @@
-package br.com.rodrigoeduque.bookstore.controller.exceptions;public class ValidationError {
+package br.com.rodrigoeduque.bookstore.controller.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+
+  private List<FieldMessage> errors = new ArrayList<>();
+
+  public ValidationError() {
+  }
+
+  public ValidationError(Long timeStamp, Integer status, String error) {
+    super(timeStamp, status, error);
+  }
+
+  public List<FieldMessage> getErrors() {
+    return errors;
+  }
+
+  public void addErrors(String fieldName, String message) {
+    this.errors.add(new FieldMessage(fieldName,message));
+  }
 }
